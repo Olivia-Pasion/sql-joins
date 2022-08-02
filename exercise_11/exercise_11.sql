0310f531d18ff9800bcfcb7ec892f53536741087
olivia pasion <oap9820@gmail.com>
2022-08-02 16:24:34 -0700

    meets rubric requirements

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,16 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+
+SELECT film.title, COUNT(rental.rental_date)
+FROM film
+INNER JOIN inventory
+	ON inventory.film_id = film.film_id
+INNER JOIN rental
+	ON rental.inventory_id = inventory.inventory_id
+GROUP BY film.film_id
+ORDER BY COUNT DESC
+LIMIT 10;
+
+
+
